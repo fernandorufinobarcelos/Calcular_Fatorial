e5b6933e36104198f875b1f55764e9a4cccd53dd
fernandorufinobarcelos <129413508+fernandorufinobarcelos@users.noreply.github.com>
2025-05-17 02:14:58 +0000

    test: adicionar classe main com testes manuais do método fatorial

---
+++ calculoFatorial/main.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args) {
+            testarFatorial(0, 1);
+            testarFatorial(1, 1);
+            testarFatorial(3, 6);
+            testarFatorial(5, 120);
+            testarFatorial(10, 3628800);
+            testarFatorial(15, 1307674368000)
+        }
+}
